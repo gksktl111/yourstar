02595bd5cadd5654c0ea57fb189243f0708eb8bf
[gksktl111] <[gksktl111@naver.com]>
2023-04-18 00:49:48 +0900

    스프링 리액트 연동함

---
+++ src/main/java/com/example/yourstar/YourstarApplication.java
@@ -0,0 +1,13 @@
+package com.example.yourstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourstarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourstarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yourstar/YourstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yourstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourstarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
